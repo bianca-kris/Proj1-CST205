1a3873526bf2eb4dcbe5eb6acb234210feaa5811
Bianca Hernandez <hernandezbk22@gmail.com>
2016-02-11 20:57:21 -0800

    created list for colors of pixel for images, created new image, found median of colors and set to new image.

--- project1.py
+++ project1.py
@@ -23,10 +23,86 @@ pictures.append(image8)
 image9 = makePicture(folder + "9.png")
 pictures.append(image9)
 
-pprint(pictures)
+for x in range(0,9):
+  show(pictures[x])
 
-height = 557
-width = 495
+height = getHeight(pictures[0])
+width = getWidth(pictures[0])
+
+newImage = makeEmptyPicture(width, height)
+
+pixelsImage1 = getPixels(pictures[0])
+pixelsImage2 = getPixels(pictures[1])
+pixelsImage3 = getPixels(pictures[2])
+pixelsImage4 = getPixels(pictures[3])
+pixelsImage5 = getPixels(pictures[4])
+pixelsImage6 = getPixels(pictures[5])
+pixelsImage7 = getPixels(pictures[6])
+pixelsImage8 = getPixels(pictures[7])
+pixelsImage9 = getPixels(pictures[8])
+
+newImagePixels = getPixels(newImage)
+
+for x in range(0,(height * width)):
+  redList = []
+  greenList = []
+  blueList = []
+  
+  pixel = pixelsImage1[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  pixel = pixelsImage2[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  pixel = pixelsImage3[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  pixel = pixelsImage4[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  pixel = pixelsImage5[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  pixel = pixelsImage6[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  pixel = pixelsImage7[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  pixel = pixelsImage8[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  pixel = pixelsImage9[x]
+  redList.append(getRed(pixel))
+  greenList.append(getGreen(pixel))
+  blueList.append(getBlue(pixel))
+  
+  redList.sort()
+  greenList.sort()
+  blueList.sort()
+  
+  setRed(newImagePixels[x], redList[4])
+  setGreen(newImagePixels[x], greenList[4])
+  setBlue(newImagePixels[x], blueList[4])
+  
+show(newImage) 
+
 
 
 
