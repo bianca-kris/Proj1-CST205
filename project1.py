387213d5e34d89beee4ee969fddc9e2734eda6b6
Bianca Hernandez <hernandezbk22@gmail.com>
2016-02-11 20:20:46 -0800

    put images into a list in .py file

--- project1.py
+++ project1.py
@@ -1,1 +1,35 @@
-print "Hello World"
+from pprint import pprint
+
+folder = "/home/iammint/coding/205-CST/Proj1/Project1Images/"
+
+pictures = []
+
+image1 = makePicture(folder + "1.png")
+pictures.append(image1)
+image2 = makePicture(folder + "2.png")
+pictures.append(image2)
+image3 = makePicture(folder + "3.png")
+pictures.append(image3)
+image4 = makePicture(folder + "4.png")
+pictures.append(image4)
+image5 = makePicture(folder + "5.png")
+pictures.append(image5)
+image6 = makePicture(folder + "6.png")
+pictures.append(image6)
+image7 = makePicture(folder + "7.png")
+pictures.append(image7)
+image8 = makePicture(folder + "8.png")
+pictures.append(image8)
+image9 = makePicture(folder + "9.png")
+pictures.append(image9)
+
+pprint(pictures)
+
+height = 557
+width = 495
+
+
+
+
+
+
